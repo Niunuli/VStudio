b8cb9c34f1ef78320c0d1230020338a4b1642f05
Niunuli <niinaperala@gmail.com>
2015-03-27 18:17:06 +0200

    kommentti koodiin

--- HelloWorld/HelloWorld/Form1.vb
+++ HelloWorld/HelloWorld/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
 
+    'Nappulaa painamalla tulee teksti
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Text = "HELLO"
     End Sub
