773c39374b67f9842ae2e38db4d1643ca8593326
Niunuli <niinaperala@gmail.com>
2015-03-27 18:57:31 +0200

    ProgressBar tutorial

---
+++ ProgressBar/ProgressBar/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ProgressBar1.Value = 25
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ProgressBar1.Value = 50
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ProgressBar1.Value = 75
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ProgressBar1.Value = 100
+    End Sub
+End Class
