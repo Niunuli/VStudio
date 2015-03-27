896d5996c13847109c8f016bda29b713865a9562
Niunuli <niinaperala@gmail.com>
2015-03-27 20:26:23 +0200

    commit multiple forms

---
+++ MultipleForms/MultipleForms/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    'https://www.youtube.com/watch?v=K46809Gw3Sc
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form2.Label2.Text = (TextBox1.Text)
+    End Sub
+End Class
