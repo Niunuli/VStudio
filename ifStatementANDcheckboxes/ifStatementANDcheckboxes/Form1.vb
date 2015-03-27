1f8c219a2dab4d3e9f2e380b9dea1e0e3503e8b4
Niunuli <niinaperala@gmail.com>
2015-03-27 21:09:15 +0200

    com

---
+++ ifStatementANDcheckboxes/ifStatementANDcheckboxes/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Me.BackColor = Color.Red 'punainen loota jos checkattu
+        Else
+            Me.BackColor = Color.Blue 'sininen jos ei
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            Button1.Enabled = True
+        Else
+            Button1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Show()
+    End Sub
+End Class
