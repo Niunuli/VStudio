bd3008edb87ae455e22ac98ff35bac321ce819d9
Niunuli <niinaperala@gmail.com>
2015-03-27 21:18:38 +0200

    tutoriaalin lähde

--- ifStatementANDcheckboxes/ifStatementANDcheckboxes/Form1.vb
+++ ifStatementANDcheckboxes/ifStatementANDcheckboxes/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
-
+    ' tutoriaali osoitteesta https://www.youtube.com/watch?v=wPtqEX6iQcQ
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
         If CheckBox1.Checked = True Then
             Me.BackColor = Color.Red 'punainen loota jos checkattu
